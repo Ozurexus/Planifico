a260962aa2cd672bf0b93bee93ac4a7c29002df4
levpen <levpen@yandex.ru>
2022-07-08 01:36:46 +0300

    button add hueta

---
+++ planner/src/routes/calendar/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    function send(){
+        
+    }
+</script>
+
+<main>
+    <button on:click="{send}">New event</button>
+    <div class="container">
+        <p>Date Time Event</p>
+
+    </div>
+</main>
+
