27ee895c7a1727c7b5412d638ebc21a6f67788b0
MaratMedvedev <freiden2004@gmail.com>
2022-07-08 02:19:20 +0300

    beautify 'New event' button

--- planner/src/routes/calendar/index.svelte
+++ planner/src/routes/calendar/index.svelte
@@ -1,14 +1,43 @@
 <script lang="ts">
-    function send(){
-        
-    }
+	function send() {}
 </script>
 
 <main>
-    <button on:click="{send}">New event</button>
-    <div class="container">
-        <p>Date Time Event</p>
-
-    </div>
+	<div class="new-event-button">
+		<button class="event-button" on:click={send}
+			><div class="button-content">
+				New event<img class="plus-image" src="plus.png" alt="plus" />
+			</div></button
+		>
+	</div>
+	<div class="container">
+		<p>Date Time Event</p>
+	</div>
 </main>
 
+<style>
+	.new-event-button {
+		margin-top: 3%;
+		display: flex;
+		justify-content: center;
+	}
+	.event-button {
+		background-color: #6d6af7;
+		display: inline;
+		color: white;
+		border: hidden;
+		padding: 5px;
+		font-size: 18px;
+		border-radius: 5px;
+	}
+	.plus-image {
+		height: 22px;
+		width: 22px;
+		margin-left: 5px;
+	}
+	.button-content {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
