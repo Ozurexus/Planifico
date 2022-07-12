95a0785aa6c108dda7ff0fba00b5696a6171a61f
bulatok <bulat2020205@gmail.com>
2022-07-12 18:24:33 +0300

    fixed bug after linter usage, which removed import

--- app/src/routes/calendar.svelte
+++ app/src/routes/calendar.svelte
@@ -1,62 +1,55 @@
 <script lang="ts">
+	import Popup from '$lib/Popup.svelte';
 	import EventDay from '$lib/eventDay.svelte';
 	import type { CalendarEvent } from '../internal/event';
 	import { getCurrentCalendar, newEventCalendar } from '../internal/out';
 	import { onMount } from 'svelte';
-	
 	import type { eventDays } from '../internal/calendar';
 	import { isAuth } from '../internal/middleware';
 	import { routes } from '../internal/config';
-	
-	let eventDays;let shown;let lastDayStr;let firstDayStr;
-
 	$: shown = false;
-	const today: Date = new Date();
+	let today: Date = new Date();
 	let firstDay: Date = new Date(today.setDate(today.getDate() - today.getDay()));
 	firstDay.setHours(0);
 	let lastDay: Date = new Date(today.setDate(today.getDate() - today.getDay() + 6));
 	$: firstDayStr = dateToString(firstDay);
 	$: lastDayStr = dateToString(lastDay);
-	const weekCofficient: number = 0;
+	let weekCofficient: number = 0;
 	// это проверка, что юзер зашел!!!
 	onMount(() => {
-	  if (!isAuth()) {
-	    location.replace(routes.basePage);
-	  }
+		if (!isAuth()) {
+			location.replace(routes.basePage);
+		}
 	});
 	$: eventDays = [];
 	onMount(async () => {
-	  let item1 = localStorage.getItem('currentAccount');
-	  const curAccount = JSON.parse(item1!);
-	  await getCurrentCalendar(curAccount).then((value) => {
-	    eventDays = value;
-	  });
+		let item1 = localStorage.getItem('currentAccount');
+		const curAccount = JSON.parse(item1!);
+		await getCurrentCalendar(curAccount).then((value) => {
+			eventDays = value;
+		});
 	});
-
 	async function addNewEvent(event: CustomEvent) {
-	  shown = false;
-	  let date: Date = new Date(event.detail.date);
-	  let {calendarEvent} = event.detail;
-
-	  let item1 = localStorage.getItem('currentAccount');
-	  const curAccount = JSON.parse(item1!);
-	  console.log(event.detail.calendarEvent);
-
-	  await newEventCalendar(curAccount, date, calendarEvent);
-	  await getCurrentCalendar(curAccount).then((value) => {
-	    eventDays = [...value];
-	    eventDays = eventDays;
-	  });
-
-	  console.log(eventDays);
-	  console.log('new event added');
+		shown = false;
+		let date: Date = new Date(event.detail.date);
+		let calendarEvent: CalendarEvent = event.detail.calendarEvent;
+		let item1 = localStorage.getItem('currentAccount');
+		const curAccount = JSON.parse(item1!);
+		console.log(event.detail.calendarEvent);
+		await newEventCalendar(curAccount, date, calendarEvent);
+		await getCurrentCalendar(curAccount).then((value) => {
+			eventDays = [...value];
+			eventDays = eventDays;
+		});
+		console.log(eventDays);
+		console.log('new event added');
 	}
 	function dateToString(date: Date): string {
-	  let dateStr: string = date.toDateString();
-	  let listDate: string[] = dateStr.split(' ');
-	  listDate[2] = listDate[2][0] !== '0' ? listDate[2] : listDate[2].slice(1);
-	  dateStr = `${listDate[1]  } ${  listDate[2]  }, ${  listDate[3]}`;
-	  return dateStr;
+		let dateStr: string = date.toDateString();
+		let listDate: string[] = dateStr.split(' ');
+		listDate[2] = listDate[2][0] !== '0' ? listDate[2] : listDate[2].slice(1);
+		dateStr = listDate[1] + ' ' + listDate[2] + ', ' + listDate[3];
+		return dateStr;
 	}
 </script>
 
@@ -87,7 +80,6 @@
 
 <style>
 	@import url('https://fonts.googleapis.com/css?family=Oswald:500,600|Lato:700,400,500,600,800');
-
 	.current-week {
 		margin-top: 2%;
 		margin-bottom: 1%;
